1804fd773c05b905042e2a0f6ba8baa4b31eb13b
Hyso Coder <kalebavincent@gmail.com>
2025-07-02 10:01:17 +0000

    new test

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,44 +1,26 @@
-import shutil
-import psutil
+import shutil, psutil
 import signal
 import os
 import asyncio
-import time
-import sys
-from sys import executable
 
 from pyrogram import idle
+from sys import executable
+
 from telegram.constants import ParseMode
-from telegram.ext import CommandHandler, Application
+from telegram.ext import CommandHandler
 from telegraph import Telegraph
 from wserver import start_server_async
-
-from bot import (
-    bot, app, application, botStartTime, IGNORE_PENDING_REQUESTS,
-    IS_VPS, PORT, alive, web, nox, OWNER_ID, AUTHORIZED_CHATS,
-    telegraph_token, LOGGER
-)
+from bot import bot, app, dispatcher, updater, botStartTime, IGNORE_PENDING_REQUESTS, IS_VPS, PORT, alive, web, nox, OWNER_ID, AUTHORIZED_CHATS, telegraph_token
 from bot.helper.ext_utils import fs_utils
 from bot.helper.telegram_helper.bot_commands import BotCommands
-from bot.helper.telegram_helper.message_utils import sendMessage, sendMarkup, editMessage, sendLogFile
-from bot.helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
-from bot.helper.telegram_helper.filters import CustomFilters
+from bot.helper.telegram_helper.message_utils import *
+from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
+from .helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper import button_build
-from telegram import InlineKeyboardMarkup
+from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, delete, speedtest, count, leech_settings, search
 
-# Import des modules
-from bot.modules import (
-    authorize, list, cancel_mirror, mirror_status, mirror,
-    clone, watch, shell, eval, delete, speedtest,
-    count, leech_settings, search
-)
 
-# Variable globale pour le serveur
-web_server = None
-# Événement global pour la gestion d'arrêt
-stop_event = asyncio.Event()
-
-async def stats(update, context):
+def stats(update, context):
     currentTime = get_readable_time(time.time() - botStartTime)
     total, used, free = shutil.disk_usage('.')
     total = get_readable_file_size(total)
@@ -51,16 +33,17 @@ async def stats(update, context):
     disk = psutil.disk_usage('/').percent
     stats = f'<b>Durée de fonctionnement du bot :</b> <code>{currentTime}</code>\n' \
             f'<b>Espace disque total :</b> <code>{total}</code>\n' \
-            f'<b>Utilisé :</b> <code>{used}</code>\n' \
+            f'<b>Utilisé :</b> <code>{used}</code>' \
             f'<b>Libre :</b> <code>{free}</code>\n\n' \
             f'<b>Upload :</b> <code>{sent}</code>\n' \
             f'<b>Download :</b> <code>{recv}</code>\n\n' \
-            f'<b>CPU :</b> <code>{cpuUsage}%</code>\n' \
-            f'<b>RAM :</b> <code>{memory}%</code>\n' \
+            f'<b>CPU :</b> <code>{cpuUsage}%</code>' \
+            f'<b>RAM :</b> <code>{memory}%</code>' \
             f'<b>DISQUE :</b> <code>{disk}%</code>'
-    await sendMessage(stats, context.bot, update)
+    sendMessage(stats, context.bot, update)
 
-async def start(update, context):
+
+def start(update, context):
     buttons = button_build.ButtonMaker()
     buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
     buttons.buildbutton("Groupe", "https://t.me/mirrorLeechTelegramBot")
@@ -70,12 +53,13 @@ async def start(update, context):
 Ce bot peut copier tous vos liens vers Google Drive !
 Tapez /{BotCommands.HelpCommand} pour obtenir la liste des commandes disponibles
 '''
-        await sendMarkup(start_string, context.bot, update, reply_markup)
+        sendMarkup(start_string, context.bot, update, reply_markup)
     else:
-        await sendMarkup('Utilisateur non autorisé', context.bot, update, reply_markup)
+        sendMarkup('Utilisateur non autorisé', context.bot, update, reply_markup)
 
-async def restart(update, context):
-    restart_message = await sendMessage("Redémarrage en cours...", context.bot, update)
+def restart(update, context):
+    restart_message = sendMessage("Redémarrage en cours...", context.bot, update)
+    # Sauvegarder le message de redémarrage pour y répondre après
     with open(".restartmsg", "w") as f:
         f.truncate(0)
         f.write(f"{restart_message.chat.id}\n{restart_message.message_id}\n")
@@ -86,29 +70,19 @@ async def restart(update, context):
         proc.kill()
     process.kill()
     nox.kill()
-
-    # Arrêt propre avant redémarrage
-    await application.updater.stop()
-    await application.stop()
-    await application.shutdown()
-    await app.stop()
-
-    # Fermer le serveur web si existant
-    global web_server
-    if IS_VPS and web_server:
-        web_server.close()
-        await web_server.wait_closed()
-
     os.execl(executable, executable, "-m", "bot")
 
-async def ping(update, context):
-    start_time = int(round(time.time() * 1000))
-    reply = await sendMessage("Début du ping", context.bot, update)
-    end_time = int(round(time.time() * 1000))
-    await editMessage(f'{end_time - start_time} ms', reply)
 
-async def log(update, context):
-    await sendLogFile(context.bot, update)
+def ping(update, context):
+    start_time = int(round(time.time() * 1000))
+    reply = sendMessage("Début du ping", context.bot, update)
+    end_time = int(round(time.time() * 1000))
+    editMessage(f'{end_time - start_time} ms', reply)
+
+
+def log(update, context):
+    sendLogFile(context.bot, update)
+
 
 help_string_telegraph = f'''<br>
 <b>/{BotCommands.HelpCommand}</b> : Obtenir ce message d'aide
@@ -119,13 +93,13 @@ help_string_telegraph = f'''<br>
 <br><br>
 <b>/{BotCommands.UnzipMirrorCommand}</b> [lien_de_téléchargement][lien_magnet] : Copier et extraire les archives vers Google Drive
 <br><br>
-<b>/{BotCommands.QbMirrorCommand}</b> [lien_magnet] : Copier en utilisant qBittorrent
+<b>/{BotCommands.QbMirrorCommand}</b> [lien_magnet] : Copier en utilisant qBittorrent, utilisez <b>/{BotCommands.QbMirrorCommand} s</b> pour sélectionner les fichiers
 <br><br>
 <b>/{BotCommands.QbZipMirrorCommand}</b> [lien_magnet] : Copier en utilisant qBittorrent et compresser (.zip)
 <br><br>
 <b>/{BotCommands.QbUnzipMirrorCommand}</b> [lien_magnet] : Copier en utilisant qBittorrent et extraire les archives
 <br><br>
-<b>/{BotCommands.LeechCommand}</b> [lien_de_téléchargement][lien_magnet] : Télécharger vers Telegram
+<b>/{BotCommands.LeechCommand}</b> [lien_de_téléchargement][lien_magnet] : Télécharger vers Telegram, utilisez <b>/{BotCommands.LeechCommand} s</b> pour sélectionner les fichiers
 <br><br>
 <b>/{BotCommands.ZipLeechCommand}</b> [lien_de_téléchargement][lien_magnet] : Télécharger vers Telegram sous forme compressée (.zip)
 <br><br>
@@ -143,7 +117,7 @@ help_string_telegraph = f'''<br>
 <br><br>
 <b>/{BotCommands.DeleteCommand}</b> [lien_drive] : Supprimer un fichier de Google Drive (Propriétaire & Sudo uniquement)
 <br><br>
-<b>/{BotCommands.WatchCommand}</b> [lien_youtube-dl] : Copier via youtube-dl
+<b>/{BotCommands.WatchCommand}</b> [lien_youtube-dl] : Copier via youtube-dl. Tapez <b>/{BotCommands.WatchCommand}</b> pour plus d'aide
 <br><br>
 <b>/{BotCommands.ZipWatchCommand}</b> [lien_youtube-dl] : Copier via youtube-dl et compresser avant envoi
 <br><br>
@@ -167,7 +141,6 @@ help_string_telegraph = f'''<br>
 <br><br>
 <b>/{BotCommands.StatsCommand}</b> : Afficher les statistiques de la machine
 '''
-
 help = Telegraph(access_token=telegraph_token).create_page(
         title='Aide Mirrorbot',
         author_name='Mirrorbot',
@@ -199,139 +172,51 @@ help_string = f'''
 /{BotCommands.ExecHelpCommand} : Aide pour le module Executor (Propriétaire uniquement)
 '''
 
-async def bot_help(update, context):
+def bot_help(update, context):
     button = button_build.ButtonMaker()
     button.buildbutton("Autres commandes", f"https://telegra.ph/{help}")
     reply_markup = InlineKeyboardMarkup(button.build_menu(1))
-    await sendMarkup(help_string, context.bot, update, reply_markup)
+    sendMarkup(help_string, context.bot, update, reply_markup)
 
-async def run_telegram_bot():
-    await application.initialize()
-    await application.start()
-    await application.updater.start_polling(drop_pending_updates=IGNORE_PENDING_REQUESTS)
-    LOGGER.info("Bot Telegram démarré")
-
-async def run_pyrogram_bot():
-    await app.start()
-    LOGGER.info("Pyrogram démarré")
-    # Utilisation d'une simple attente au lieu de idle()
-    await stop_event.wait()
-
-async def shutdown(signame=None):
-    LOGGER.info(f"Reçu signal {signame}, arrêt en cours...")
-    global web_server
-
-    # Déclencher l'événement d'arrêt
-    stop_event.set()
-
-    # Arrêt du bot Telegram
-    if application.updater and application.updater.running:
-        await application.updater.stop()
-    if application.running:
-        await application.stop()
-        await application.shutdown()
-
-    # Arrêt de Pyrogram
-    if app.is_initialized:
-        await app.stop()
-
-    # Fermer le serveur web
-    if IS_VPS and web_server:
-        web_server.close()
-        await web_server.wait_closed()
-
-    # Arrêt des autres processus
-    if alive:
-        alive.kill()
-    if web:
-        try:
-            process = psutil.Process(web.pid)
-            for proc in process.children(recursive=True):
-                proc.kill()
-            process.kill()
-        except:
-            pass
-    if nox:
-        nox.kill()
-
-    LOGGER.info("Arrêt complet réussi")
-    sys.exit(0)
-
-async def main():
-    global web_server
-
-    # Initialisation de l'application
+def main():
     fs_utils.start_cleanup()
-    loop = asyncio.get_running_loop()
-
-    # Gestion des signaux
-    for signame in ('SIGINT', 'SIGTERM'):
-        loop.add_signal_handler(
-            getattr(signal, signame),
-            lambda s=signame: asyncio.create_task(shutdown(s))
-        )
-
-    # Démarrer le serveur web
     if IS_VPS:
-        web_server = await start_server_async(PORT)
-        LOGGER.info(f"Serveur web démarré sur le port {PORT}")
-
-    # Vérification du redémarrage
+        asyncio.get_event_loop().run_until_complete(start_server_async(PORT))
+    # Vérifier si le bot redémarre
     if os.path.isfile(".restartmsg"):
         with open(".restartmsg") as f:
             chat_id, msg_id = map(int, f)
-        await application.bot.edit_message_text("Redémarré avec succès !", chat_id, msg_id)
+        bot.edit_message_text("Redémarré avec succès !", chat_id, msg_id)
         os.remove(".restartmsg")
     elif OWNER_ID:
         try:
             text = "<b>Bot redémarré !</b>"
-            await application.bot.send_message(chat_id=OWNER_ID, text=text, parse_mode=ParseMode.HTML)
+            bot.sendMessage(chat_id=OWNER_ID, text=text, parse_mode=ParseMode.HTML)
             if AUTHORIZED_CHATS:
                 for i in AUTHORIZED_CHATS:
-                    await application.bot.send_message(chat_id=i, text=text, parse_mode=ParseMode.HTML)
+                    bot.sendMessage(chat_id=i, text=text, parse_mode=ParseMode.HTML)
         except Exception as e:
             LOGGER.warning(e)
-
-    # Configuration des handlers
-    start_handler = CommandHandler(BotCommands.StartCommand, start)
+    start_handler = CommandHandler(BotCommands.StartCommand, start, run_async=True)
     ping_handler = CommandHandler(BotCommands.PingCommand, ping,
-                                filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
+                                  filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
     restart_handler = CommandHandler(BotCommands.RestartCommand, restart,
-                                   filters=CustomFilters.owner_filter | CustomFilters.sudo_user)
-    help_handler = CommandHandler(BotCommands.HelpCommand, bot_help,
-                                filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
-    stats_handler = CommandHandler(BotCommands.StatsCommand, stats,
-                                 filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
-    log_handler = CommandHandler(BotCommands.LogCommand, log,
-                               filters=CustomFilters.owner_filter | CustomFilters.sudo_user)
+                                     filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+    help_handler = CommandHandler(BotCommands.HelpCommand,
+                                  bot_help, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+    stats_handler = CommandHandler(BotCommands.StatsCommand,
+                                   stats, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+    log_handler = CommandHandler(BotCommands.LogCommand, log, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(ping_handler)
+    dispatcher.add_handler(restart_handler)
+    dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(stats_handler)
+    dispatcher.add_handler(log_handler)
+    updater.run_polling(drop_pending_updates=IGNORE_PENDING_REQUESTS)
+    LOGGER.info("Bot démarré !")
+    signal.signal(signal.SIGINT, fs_utils.exit_clean_up)
 
-    application.add_handler(start_handler)
-    application.add_handler(ping_handler)
-    application.add_handler(restart_handler)
-    application.add_handler(help_handler)
-    application.add_handler(stats_handler)
-    application.add_handler(log_handler)
-
-    # Démarrer les bots en parallèle
-    telegram_task = asyncio.create_task(run_telegram_bot())
-    pyrogram_task = asyncio.create_task(run_pyrogram_bot())
-
-    # Attendre que les tâches se terminent
-    await asyncio.gather(telegram_task, pyrogram_task)
-
-if __name__ == '__main__':
-    # Création d'une nouvelle boucle d'événements
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-    try:
-        loop.run_until_complete(main())
-    except (KeyboardInterrupt, SystemExit):
-        LOGGER.info("Bot arrêté par l'utilisateur")
-    except Exception as e:
-        LOGGER.error(f"Erreur inattendue: {e}")
-    finally:
-        # Exécuter la procédure d'arrêt dans la boucle
-        if not stop_event.is_set():
-            loop.run_until_complete(shutdown())
-        loop.close()
+app.start()
+main()
+idle()
